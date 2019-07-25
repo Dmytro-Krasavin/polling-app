6a8526a8d32d38d97005d025ea66a05f99512b21
Krasavin Dmitry <krasavin.d.96@gmail.com>
2019-07-25 15:17:06 +0300

    made JwtAuthenticationResponse immutable

--- src/main/java/com/example/polls/payload/response/JwtAuthenticationResponse.java
+++ src/main/java/com/example/polls/payload/response/JwtAuthenticationResponse.java
@@ -1,15 +1,13 @@
 package com.example.polls.payload.response;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class JwtAuthenticationResponse {
 
-    private String accessToken;
+    private final String accessToken;
 
-    private String tokenType = "Bearer";
+    private final String tokenType = "Bearer";
 
     public JwtAuthenticationResponse(String accessToken) {
         this.accessToken = accessToken;

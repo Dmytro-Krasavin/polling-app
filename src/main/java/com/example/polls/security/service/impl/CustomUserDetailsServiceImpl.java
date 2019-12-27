44eda5c14e6bb862b3b3a4ae40102873d9ab6e9b
Krasavin Dmitry <krasavin.d.96@gmail.com>
2019-12-27 15:57:24 +0200

    Add CheckClientIP annotation

--- src/main/java/com/example/polls/security/service/impl/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/example/polls/security/service/impl/CustomUserDetailsServiceImpl.java
@@ -1,17 +1,14 @@
 package com.example.polls.security.service.impl;
 
-import com.example.polls.exception.ForbiddenException;
 import com.example.polls.model.User;
 import com.example.polls.repository.UserRepository;
+import com.example.polls.security.annotation.CheckClientIP;
 import com.example.polls.security.model.UserPrincipal;
 import com.example.polls.security.service.CustomUserDetailsService;
-import com.example.polls.security.service.LoginAttemptService;
-import com.example.polls.util.IpAddressExtractor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,31 +16,19 @@ public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
 
     private final UserRepository userRepository;
 
-    private final LoginAttemptService loginAttemptService;
-
-    private final IpAddressExtractor ipAddressExtractor;
-
     @Override
+    @CheckClientIP
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
-        checkClientIP();
         User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail));
         return UserPrincipal.create(user);
     }
 
     @Override
-    @Transactional
+    @CheckClientIP
     public UserDetails loadUserById(Long id) {
-        checkClientIP();
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with id : " + id));
         return UserPrincipal.create(user);
-    }
-
-    private void checkClientIP() {
-        String ip = ipAddressExtractor.getClientIP();
-        if (loginAttemptService.isBlocked(ip)) {
-            throw new ForbiddenException("Client IP address is blocked");
-        }
     }
 }
